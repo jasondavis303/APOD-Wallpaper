7a41db472bbd22f24e5bbd745d6d055bed7682a1
Jason Davis <jasondavis303@gmail.com>
2025-06-26 16:28:12 -0600

    Use official api, and use windows com since the pinvoke stopped working

---
+++ APOD Wallpaper/APODResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace APOD_Wallpaper;
+
+internal class APODResponse
+{
+    //[JsonPropertyName("copyright")]
+    //public string Copyright { get; set; }
+
+    //[JsonPropertyName("date")]
+    //public DateTimeOffset Date { get; set; }
+
+    //[JsonPropertyName("explanation")]
+    //public string Explanation { get; set; }
+
+    [JsonPropertyName("hdurl")]
+    public string Hdurl { get; set; }
+
+    //[JsonPropertyName("media_type")]
+    //public string MediaType { get; set; }
+
+    //[JsonPropertyName("service_version")]
+    //public string ServiceVersion { get; set; }
+
+    //[JsonPropertyName("title")]
+    //public string Title { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
--- APOD Wallpaper/MyAppContext.cs
+++ APOD Wallpaper/MyAppContext.cs
@@ -2,114 +2,116 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using WebUtils;
 
-namespace APOD_Wallpaper
+namespace APOD_Wallpaper;
+
+internal class MyAppContext : ApplicationContext
 {
-    internal class MyAppContext : ApplicationContext
+    const string APOD_URL = "https://apod.nasa.gov/apod/";
+
+    private readonly NotifyIcon _trayIcon = new()
     {
-        const string APOD_URL = "https://apod.nasa.gov/apod/";
+        Icon = Properties.Resources.star,
+        Visible = true
+    };
 
-        [DllImport("User32", CharSet = CharSet.Auto)]
-        public static extern void SystemParametersInfo(int uiAction, int uiParam, string pvParam, uint fWinIni);
+    private static readonly string[] _validImageExtensions = new string[] { ".jpg", ".png", ".bmp" };
+
+    private readonly System.Threading.Timer _timer;
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+    private readonly CancellationToken _cancellationToken;
+    private readonly HttpClient _httpClient = new();
+    
+    public MyAppContext()
+    {
+        _cancellationToken = _cancellationTokenSource.Token;
+
+        //Run on login
+        using var key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        key.SetValue(Application.ProductName, Application.ExecutablePath);
+
+
+        //Menu
+        var cms = _trayIcon.ContextMenuStrip = new ContextMenuStrip();
+
+        cms.Items.Add("APOD").Click += APOD_Menu_Clicked;
+        cms.Items.Add("-");
+        cms.Items.Add("Exit").Click += Exit_Menu_Click;
+
+        _timer = new System.Threading.Timer(new TimerCallback(Timer_Ticked), null, 0, Timeout.Infinite);
+    }
+
+    private void APOD_Menu_Clicked(object sender, EventArgs e)
+    {
+        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(APOD_URL) { UseShellExecute = true });   
+    }
+
+    private void Exit_Menu_Click(object sender, EventArgs e)
+    {
+        _timer.Dispose();
+        _trayIcon.Dispose();
+        _cancellationTokenSource.Cancel();
+        Application.Exit();
+    }
+
+    private async void Timer_Ticked(object state)
+    {
+        try { await DoWork(); }
+        catch { }
+
+        //Try block just incase the timer is disposed
+        try
+        {
+            if (!_cancellationTokenSource.IsCancellationRequested)
+                _timer.Change(1000 * 60 * 60, Timeout.Infinite);
+        }
+        catch { }
+    }
+
+
+    private async Task DoWork()
+    {
+        using var key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\DustyPig.tv\\APOD Wallpaper", true);
+
+        string lastUrl = null;
+        try { lastUrl = key.GetValue("LastUrl", string.Empty) as string; }
+        catch { }
+
+        string apiUrl = "https://api.nasa.gov/planetary/apod?api_key=dT4APYOJBWpbHdA5GkvmglXwhvMgaSGXdnJK1cyg";
+
+        string currentUrl = null;
+        var apodResponse = await _httpClient.GetFromJsonAsync<APODResponse>(apiUrl, _cancellationToken).ConfigureAwait(false);
+        currentUrl = apodResponse.Hdurl;
+        currentUrl ??= apodResponse.Url;
+        if (string.IsNullOrWhiteSpace(currentUrl))
+            return;
+        if (currentUrl == lastUrl)
+            return;
+
+
+        string ext = (Path.GetExtension(currentUrl) + string.Empty).ToLower();
+        if (!_validImageExtensions.Contains(ext))
+            return;
+        string imageFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "DustyPig.tv", "APOD Wallpaper");
+        Directory.CreateDirectory(imageFile);
+        imageFile = Path.Combine(imageFile, "apod_image" + ext);
         
-        private readonly NotifyIcon _trayIcon = new()
-        {
-            Icon = Properties.Resources.star,
-            Visible = true
-        };
 
-        private readonly System.Threading.Timer _timer;
-        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-        private readonly CancellationToken _cancellationToken;
+        using var httpResponse = await _httpClient.GetAsync(currentUrl, HttpCompletionOption.ResponseHeadersRead, _cancellationToken).ConfigureAwait(false);
+        httpResponse.EnsureSuccessStatusCode();
+        using var content = await httpResponse.Content.ReadAsStreamAsync(_cancellationToken).ConfigureAwait(false);
 
-        
-        public MyAppContext()
-        {
-            _cancellationToken = _cancellationTokenSource.Token;
+        using var fileStream = new FileStream(imageFile, FileMode.Create, FileAccess.Write, FileShare.Read, 4096, true);
+        await content.CopyToAsync(fileStream).ConfigureAwait(false);
 
-            //Run on login
-            var key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-            key.SetValue(Application.ProductName, Application.ExecutablePath);
-            key.Dispose();
-
-
-            //Menu
-            var cms = _trayIcon.ContextMenuStrip = new ContextMenuStrip();
-
-            cms.Items.Add("APOD").Click += APOD_Menu_Clicked;
-            cms.Items.Add("-");
-            cms.Items.Add("Exit").Click += Exit_Menu_Click;
-
-            _timer = new System.Threading.Timer(new TimerCallback(Timer_Ticked), null, 0, Timeout.Infinite);
-        }
-
-        private void APOD_Menu_Clicked(object sender, EventArgs e)
-        {
-            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(APOD_URL) { UseShellExecute = true });   
-        }
-
-        private void Exit_Menu_Click(object sender, EventArgs e)
-        {
-            _timer.Dispose();
-            _trayIcon.Dispose();
-            _cancellationTokenSource.Cancel();
-            Application.Exit();
-        }
-
-        private async void Timer_Ticked(object state)
-        {
-            try { await DoWork(); }
-            catch { }
-
-            //Try block just incase the timer is disposed
-            try
-            {
-                if (!_cancellationTokenSource.IsCancellationRequested)
-                    _timer.Change(1000 * 60 * 60, Timeout.Infinite);
-            }
-            catch { }
-        }
-
-
-        private async Task DoWork()
-        {
-            string trackingFile = Path.Combine(Path.GetTempPath(),  "current_apod_image.txt");
-
-            string lastUrl = null;
-            try { lastUrl = File.ReadAllText(trackingFile); }
-            catch { }
-
-            try
-            {
-                string html = await SimpleDownloader.DownloadStringAsync(APOD_URL, _cancellationToken).ConfigureAwait(false);
-                string url = html.Substring(html.IndexOf("<a href=\"image/") + 9);
-                url = url.Substring(0, url.IndexOf("\""));
-                url = APOD_URL + url;
-
-                if (url != lastUrl)
-                {
-                    string ext = (Path.GetExtension(url) + string.Empty).ToLower();
-                    if (new string[] { ".jpg", ".png", ".bmp" }.Contains(ext))
-                    {
-                        string imgeFile = Path.Combine(Path.GetTempPath(), "apod_image" + ext);
-                        if (File.Exists(imgeFile))
-                            File.Delete(imgeFile);
-
-                        await SimpleDownloader.DownloadFileAsync(url, imgeFile, _cancellationToken).ConfigureAwait(false);
-
-                        SystemParametersInfo(0x0014, 0, imgeFile, 0x0001);
-                    }
-
-                    File.WriteAllText(trackingFile, url);
-                }
-            }
-            catch { }
-
-        }
+        key.SetValue("LastUrl", currentUrl);
+        var wallpaper = (IDesktopWallpaper)(new DesktopWallpaperClass());
+        wallpaper.SetWallpaper(null, imageFile);
+        wallpaper.Enable();
     }
 }
